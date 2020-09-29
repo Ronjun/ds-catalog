a3e9a72ee3abc3709995b55d348bcc8ff5777e89
Ronaldo Junior <ronaldocosta2603@gmail.com>
2020-09-29 10:23:14 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Smartphones');
+INSERT INTO tb_category (name) VALUES ('Hardware');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Games');
+INSERT INTO tb_category (name) VALUES ('Consoles');
+INSERT INTO tb_category (name) VALUES ('Movies');
+INSERT INTO tb_category (name) VALUES ('Toys');
