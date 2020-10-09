b5b445e509629fc2407a60f520a84e8f637b244f
Ronaldo Junior <ronaldocosta2603@gmail.com>
2020-10-09 10:16:06 -0300

    catalog page

--- front-web/src/core/components/button-icon/index.tsx
+++ front-web/src/core/components/button-icon/index.tsx
@@ -6,7 +6,7 @@ type Props = {
     text: string;
 }
 const ButtonIcon = ({text}: Props) => (
-    <div className="d-flex">
+    <div className="d-inline-flex">
         <button className="btn btn-primary btn-icon">
             <h5>{text}</h5>
         </button>
