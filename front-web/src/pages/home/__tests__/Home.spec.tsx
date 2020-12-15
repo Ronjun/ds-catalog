791ad967bac7de62ae449931026474b2343b5753
Ronaldo Junior <ronaldocosta2603@gmail.com>
2020-12-15 07:56:36 -0300

    Add home tests

---
+++ front-web/src/pages/home/__tests__/Home.spec.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "..";
+import { Router } from "react-router-dom";
+import history from "core/utils/history";
+
+test("should render home", () => {
+  render(
+    <Router history={history}>
+      <Home />
+    </Router>
+  );
+
+  const titleElement = screen.getByText("Conheça o melhor catálogo de produtos");
+  const subtitleElement = screen.getByText("Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.")
+
+  expect(titleElement).toBeInTheDocument();
+  expect(subtitleElement).toBeInTheDocument();
+  expect(screen.getByTestId("main-image")).toBeInTheDocument();
+  expect(screen.getByText(/INICIE AGORA A SUA BUSCA/i)).toBeInTheDocument();
+});
